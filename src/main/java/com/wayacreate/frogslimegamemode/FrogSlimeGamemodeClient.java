e42176c5accfc5b957c531fd3d209dce40d65e3c
WayaSteurbautYT <steurbautwaya703@gmail.com>
2026-04-26 23:17:42 +0200

    Register entity renderers in client initialization
    Fixes NullPointerException during Iris shader shadow rendering by properly registering all custom entity renderers.

--- src/main/java/com/wayacreate/frogslimegamemode/FrogSlimeGamemodeClient.java
+++ src/main/java/com/wayacreate/frogslimegamemode/FrogSlimeGamemodeClient.java
@@ -2,8 +2,14 @@ package com.wayacreate.frogslimegamemode;
 
 import com.wayacreate.frogslimegamemode.client.hud.GamemodeHud;
 import com.wayacreate.frogslimegamemode.client.keybind.ModKeybinds;
+import com.wayacreate.frogslimegamemode.entity.ModEntities;
+import com.wayacreate.frogslimegamemode.entity.client.FrogHelperRenderer;
+import com.wayacreate.frogslimegamemode.entity.client.FrogKingRenderer;
+import com.wayacreate.frogslimegamemode.entity.client.GiantSlimeBossRenderer;
+import com.wayacreate.frogslimegamemode.entity.client.SlimeHelperRenderer;
 import com.wayacreate.frogslimegamemode.network.ModNetworkingClient;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class FrogSlimeGamemodeClient implements ClientModInitializer {
     @Override
@@ -13,8 +19,11 @@ public class FrogSlimeGamemodeClient implements ClientModInitializer {
         ModNetworkingClient.registerClient();
         ModKeybinds.register();
         
-        // Entity renderers will be registered when the renderer classes are implemented
-        // For now, we'll use vanilla renderers as fallback
+        // Register entity renderers
+        EntityRendererRegistry.register(ModEntities.FROG_HELPER, FrogHelperRenderer::new);
+        EntityRendererRegistry.register(ModEntities.SLIME_HELPER, SlimeHelperRenderer::new);
+        EntityRendererRegistry.register(ModEntities.GIANT_SLIME_BOSS, GiantSlimeBossRenderer::new);
+        EntityRendererRegistry.register(ModEntities.FROG_KING, FrogKingRenderer::new);
         
         FrogSlimeGamemode.LOGGER.info("Frog & Slime Gamemode client initialized!");
     }
